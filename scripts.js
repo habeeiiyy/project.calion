5ec8781ffae840db7e0f8136af073260bb033d41
habeeiiyy <habeebpalanthodiyil0@gmail.com>
2025-09-08 22:19:30 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Wait for the page to load
-window.addEventListener("load", () => {
-  // Optional minimum wait time (in ms)
-  setTimeout(() => {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("main-content").style.display = "block";
-  }, 2000); // 2 seconds delay
-});
